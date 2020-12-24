d7d9140bd4f92d500e3f1506796b79d0819c429d
madhuvayya <madhu.vayya@gmail.com>
2020-12-24 22:42:04 +0530

    Added code to bind the data in component class to html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello from Bridgelabz';
+  title = 'HelloWorld';
+
+  ngOnInit(){
+    this.title = 'Hello from Bridgelabz';
+  }
 }
